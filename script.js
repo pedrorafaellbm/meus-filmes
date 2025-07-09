e8f79df0ac28322ce74ed27bdb89ea066506c9c5
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-07-08 21:36:47 -0300

    primeira parte com uma api propria

---
+++ script.js
@@ -0,0 +1,13 @@
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer yJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZjAxMjZkNjQ2OTU4ZWM4MzJmYTM3NGFjOGQ3MDhlNSIsIm5iZiI6MTc1MjAxODY1OS4xNTIsInN1YiI6IjY4NmRhZWUzMzY0YmVmMDM4NGNlZWQyYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9fobntkbwah6K5MyC5II7J9QYLuBanDYbB1HMIHyNW8`
+    }
+}
+
+fetch(`https://api.themoviedb.org/3/authentication`, options)
+.then(response => response.json())
+.then(data =>{
+    console.log(data)
+})
